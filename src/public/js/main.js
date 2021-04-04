211937f0fc16d199af4b3de32ca33995211fae95
Raymond shi <rs5955@nyu.edu>
2021-04-04 00:45:09 -0400

    Implement dispensing

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,16 +1,28 @@
 const SODA_API = "http://localhost:3000/api/sodas";
-
+let sodaSelection = undefined;
+let isBusy = false; //flag to disable soda selection if machine is currently dispensing
 
 function test(){
     console.log("HERE");
 }
 
+function updateStatusBar(content){
+    console.log(content);
+    const statusBar = document.querySelector("#statusBar");
+    statusBar.textContent = content;
+}
+
 function addSoda(soda){
     const toAdd = document.createElement("div");
     toAdd.classList.add("soda");
-    toAdd.textContent = "Hey";
+    toAdd.textContent = soda.name;
     toAdd.addEventListener("click",()=>{
-        console.log("here");
+        if(!isBusy){
+            updateStatusBar(soda.name);
+            sodaSelection = soda;
+        }else{
+            console.log("IM BUSY");
+        }
     });
     document.querySelector("#sodaList").appendChild(toAdd);
     console.log(soda);
@@ -27,25 +39,46 @@ function loadSodas(){
             addSoda(s);
         });
     });
-    
-    
-    
 }
 
-function loadUserInput(){
-    console.log("Here");
-    
+//helper fn to add some delay
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+//this fn is only called when soda selection !== undefined or null
+function getSoda(soda){
+    //implement soda json download
+}
+
+function setGetButton(){
+    const getBtn = document.querySelector("#getButton");
+    getBtn.addEventListener('click', async ()=>{
+        if(!sodaSelection){
+            updateStatusBar("Please Select a Soda");  
+        }else{
+            updateStatusBar("Dispensing soda...");
+            isBusy = true;
+            await sleep(1000);
+            
+            getSoda(sodaSelection);
+            updateStatusBar("Thank You!");
+            
+            await sleep(2000);
+            updateStatusBar("SELECT A DRINK");
+            isBusy = false;
+        }
+    });
 }
 
 function handleLoad(){
     //loads the sodas into the machine
     loadSodas();
-    loadUserInput();
+    setGetButton();
 }
 
 function main(){
     handleLoad();
-    
 }
 
 document.addEventListener("DOMContentLoaded",main);
